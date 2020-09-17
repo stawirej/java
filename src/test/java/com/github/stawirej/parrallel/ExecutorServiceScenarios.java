0fe1803f2e515db4bede0ca45c2f496ddd5ac6a7
Piotr Stawirej <piotr.stawirej@gmail.com>
2020-09-17 22:43:48 +0200

    Init commit.

---
+++ src/test/java/com/github/stawirej/parrallel/ExecutorServiceScenarios.java
@@ -0,0 +1,18 @@
+package com.github.stawirej.parrallel;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+final class ExecutorServiceScenarios {
+
+    @Test
+    void foo(){
+        // Given
+
+        // When
+
+        // Then
+    }
+}
